73ced6ad36f43e605a2a0cb294886a80fb05d2cf
nikolay.devyaty <nikolay.devyaty@fintopio.com>
2024-04-29 04:50:02 +0300

    added app.py

--- bot.py
+++ bot.py
@@ -1,34 +1,50 @@
 import asyncio
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, Router
 from aiogram.enums import ParseMode
+from aiogram.types import Message, WebAppInfo
+from aiogram.filters import CommandStart
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-from motor.motor_asyncio import AsyncIOMotorClient
+# from motor.motor_asyncio import AsyncIOMotorClient
 
-from handlers import setup_message_routers
-from callbacks import setup_callbacks_routers
+# from handlers import setup_message_routers
+# from callbacks import setup_callbacks_routers
 
-from middlewares import CheckUser
-
-from config_reader import config
-
-
-async def main() -> None:
-    bot = Bot(config.BOT_TOKEN.get_secret_value(), parse_mode=ParseMode.HTML)
-    dp = Dispatcher()
-
-    cluster = AsyncIOMotorClient(config.DATABASE_URL.get_secret_value())
-    db = cluster.anonimdb
-
-    dp.message.middleware(CheckUser())
-
-    message_routers = setup_message_routers()
-    callback_routers = setup_callbacks_routers()
-    dp.include_router(message_routers)
-    dp.include_router(callback_routers)
-
-    await dp.start_polling(bot, db=db)
+# from middlewares import CheckUser
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+# from config_reader import config
+def webapp_builder() -> InlineKeyboardBuilder:
+    builder = InlineKeyboardBuilder()
+    builder.button(text="lets Click!", web_app=WebAppInfo(url="..."))
+    return builder.as_markup()
+
+
+router = Router()
+
+
+@router.message(CommandStart())
+async def start(message: Message) -> None:
+    await message.reply("Click Click", reeply_markup=webapp_builder())
+
+
+# async def main() -> None:
+#     bot = Bot(config.BOT_TOKEN.get_secret_value(), parse_mode=ParseMode.HTML)
+#     dp = Dispatcher()
+
+#     cluster = AsyncIOMotorClient(config.DATABASE_URL.get_secret_value())
+#     db = cluster.anonimdb
+
+#     dp.message.middleware(CheckUser())
+
+#     message_routers = setup_message_routers()
+#     callback_routers = setup_callbacks_routers()
+#     dp.include_router(message_routers)
+#     dp.include_router(callback_routers)
+
+#     await dp.start_polling(bot, db=db)
+
+
+# if __name__ == "__main__":
+#     asyncio.run(main())
