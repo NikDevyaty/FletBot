064f706edeb823fbb80d60516edb1d410088f52b
nikolay.devyaty <nikolay.devyaty@fintopio.com>
2024-05-01 16:11:27 +0300

    comments code removed

--- bot.py
+++ bot.py
@@ -6,15 +6,6 @@ from aiogram.types import Message, WebAppInfo
 from aiogram.filters import CommandStart
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-# from motor.motor_asyncio import AsyncIOMotorClient
-
-# from handlers import setup_message_routers
-# from callbacks import setup_callbacks_routers
-
-# from middlewares import CheckUser
-
-
-# from config_reader import config
 def webapp_builder() -> InlineKeyboardBuilder:
     builder = InlineKeyboardBuilder()
     builder.button(
@@ -37,14 +28,6 @@ async def main() -> None:
         "7000256880:AAHidXEOByPasXkppLRL5ZseH74fLQGolYw", parse_mode=ParseMode.HTML
     )
     dp = Dispatcher()
-
-    # cluster = AsyncIOMotorClient(config.DATABASE_URL.get_secret_value())
-    # db = cluster.anonimdb
-
-    # dp.message.middleware(CheckUser())
-
-    # message_routers = setup_message_routers()
-    # callback_routers = setup_callbacks_routers()
     dp.include_router(router)
     await bot.delete_webhook(True)
     await dp.start_polling(bot)
